d075213632d82d683ca71347eae65cac1a117218
manuel1280 <komodocr7@gmail.com>
2019-09-10 08:33:19 -0500

    add field and validations to time posted

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,5 @@ class Micropost < ApplicationRecord
     belongs_to :user
 
     validates :content, length: { maximum: 140, too_long: "%{count} characters is the maximum allowed. "}
+    validates :time_posted, numericality: { only_integer: true }
 end
