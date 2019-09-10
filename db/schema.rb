d389a0c35d9e6df9fbb9b6bf7977a4b5544e8472
manuel1280 <komodocr7@gmail.com>
2019-09-10 08:12:28 -0500

    add migrations of new fields for expired posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_08_215327) do
+ActiveRecord::Schema.define(version: 2019_09_10_130457) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "time_posted"
+    t.datetime "expiration_date"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,6 +27,11 @@ ActiveRecord::Schema.define(version: 2019_09_08_215327) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
